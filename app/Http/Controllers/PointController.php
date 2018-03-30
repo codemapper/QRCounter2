8ac5af8aa15615e8b8e0e682c21a9c1b3ecebd78
Kurt Blaser <kurt.blaser@bbcag.ch>
2018-03-30 20:26:01 +0200

    QRScan added

--- app/Http/Controllers/PointController.php
+++ app/Http/Controllers/PointController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class PointController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
